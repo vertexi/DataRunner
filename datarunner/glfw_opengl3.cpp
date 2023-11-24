679921adcccf8e51736840f46adfb20acd400c93
Vertexi <778950965@qq.com>
2023-11-24 16:40:35 +0800

    change imgui config from vcpkg to manual for flexibility.

--- datarunner/glfw_opengl3.cpp
+++ datarunner/glfw_opengl3.cpp
@@ -33,7 +33,7 @@ int window_init()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    // glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
     // glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
