89b960c73fb632135599062f6a88945521e718ba
Vertexi <778950965@qq.com>
2023-12-01 12:18:32 +0800

    start port my app.

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,2 @@
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=%userprofile%/Documents/My_Files/Git_repos/vcpkg/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 17 2022"
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DIMGUI_BUNDLE_WITH_IMMVISION=OFF -DIMGUI_BUNDLE_WITH_TEST_ENGINE=OFF -DCMAKE_TOOLCHAIN_FILE=%userprofile%/Documents/My_Files/Git_repos/vcpkg/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 17 2022"
 cmake --build build
